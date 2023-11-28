7e31b95f35ce7a3612a4a293e29a981eea491179
Michael Khanda <michaelkhanda006@gmail.com>
2023-11-28 16:16:56 +0300

    updated commit

---
+++ emobilisFinalProject/main_app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def confirmation(request):
+    return render(request, 'confirmation.html')
+
+
+def appointment(request):
+    return render(request, 'appoinment.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def department(request):
+    return render(request, 'department.html')
+
+
+def department_single(request):
+    return render(request, 'department-single.html')
+
+
+def doctor(request):
+    return render(request, 'doctor.html')
+
+
+def doctor_single(request):
+    return render(request, 'doctor-single.html')
+
+
+def service(request):
+    return render(request, 'service.html')
